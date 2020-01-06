cab46d51bde492e0f0ed33bbe94634e257f7def1
h3li05369 <akashsm@ce.iitr.ac.in>
2020-01-07 00:58:50 +0530

    Basic GRU model implemented

--- tfp/models/seq2seq.py
+++ tfp/models/seq2seq.py
@@ -26,40 +26,23 @@ class Seq2SeqModel(nn.Module):
 
     def __init__(self,
                  architecture,
-                 source_seq_len,
-                 target_seq_len,
                  rnn_size,  # hidden recurrent layer size
                  num_layers,
-                 max_gradient_norm,
-                 batch_size,
-                 learning_rate,
-                 learning_rate_decay_factor,
-                 loss_to_use,
                  num_joints=21,
-                 one_hot=False,
                  residual_velocities=False,
                  dropout=0.0,
-                 teacher_ratio=0.0,
-                 dtype=torch.float32):
+                 teacher_ratio=0.0):
         """Create the model.
         Args:
           architecture: [basic, tied] whether to tie the encoder and decoder.
-          source_seq_len: lenght of the input sequence.
-          target_seq_len: lenght of the target sequence.
           rnn_size: number of units in the rnn.
           num_layers: number of rnns to stack.
-          max_gradient_norm: gradients will be clipped to maximally this norm
-          one_hot: whether to use one_hot encoding during train/test (sup models).
           residual_velocities: whether to use a residual connection that models velocities.
-          dtype: the data type to use to store internal variables.
         """
         super(Seq2SeqModel, self).__init__()
 
         self.input_size = num_joints*3
         # Summary writers for train and test runs
-
-        self.source_seq_len = source_seq_len
-        self.target_seq_len = target_seq_len
         self.rnn_size = rnn_size
         self.dropout = nn.Dropout(dropout)
         self.residual_velocities = residual_velocities
@@ -69,27 +52,32 @@ class Seq2SeqModel(nn.Module):
         self.decoder = torch.nn.GRUCell(
             self.rnn_size, self.rnn_size)
 
-        self.fc1 = nn.Linear(self.rnn_size, self.input_size)
+        self.projector = nn.Linear(self.rnn_size, self.input_size)
 
     def forward(self, encoder_inputs, decoder_inputs):
         """
         Args:
             encoder_inputs: batch of dance pose sequences , shape=(batch_size,source_seq_length,num_joints*3)
             decoder_inputs: batch of dance pose sequences , shape=(batch_size,target_seq_length-1,num_joints*3)
+        Returns:
+            outputs: batch of predicted dance pose sequences, shape=(batch_size,target_seq_length-1,num_joints*3)
         """
         # First calculate the encoder hidden state
         all_hidden_states, (encoder_hidden_state, encoder_cell_state) = self.encoder(
             encoder_inputs)
 
         outputs = []
+        # Iterate over decoder inputs
         for inp in decoder_inputs:
+            # Perform teacher forcing
             if random.random() < self.teacher_forcing:
                 inp = prev_output
             next_state = self.decoder(inp, encoder_hidden_state)
+            # Apply residual network to help in smooth transition between subsequent poses
             if self.residual_velocities:
                 output = inp + self.fc1(self.dropout(next_state))
             else:
-                output = self.fc1(self.dropout(next_state))
+                output = self.projector(self.dropout(next_state))
             # Store the output for Teacher Forcing: use the prediction as
             # the next input instead of feeding the ground truth
             prev_output = output
@@ -98,138 +86,3 @@ class Seq2SeqModel(nn.Module):
 
         outputs = torch.cat(outputs, 0)
         return torch.transpose(outputs, 0, 1)
-
-    def get_batch(self, data, actions):
-        """Get a random batch of data from the specified bucket, prepare for step.
-        Args
-          data: a list of sequences of size n-by-d to fit the model to.
-          actions: a list of the actions we are using
-        Returns
-          The tuple (encoder_inputs, decoder_inputs, decoder_outputs);
-          the constructed batches have the proper format to call step(...) later.
-        """
-
-        # Select entries at random
-        all_keys = list(data.keys())
-        chosen_keys = np.random.choice(len(all_keys), self.batch_size)
-
-        # How many frames in total do we need?
-        total_frames = self.source_seq_len + self.target_seq_len
-
-        encoder_inputs = np.zeros(
-            (self.batch_size, self.source_seq_len-1, self.input_size), dtype=float)
-        decoder_inputs = np.zeros(
-            (self.batch_size, self.target_seq_len, self.input_size), dtype=float)
-        decoder_outputs = np.zeros(
-            (self.batch_size, self.target_seq_len, self.input_size), dtype=float)
-
-        for i in xrange(self.batch_size):
-
-            the_key = all_keys[chosen_keys[i]]
-
-            # Get the number of frames
-            n, _ = data[the_key].shape
-
-            # Sample somewherein the middle
-            idx = np.random.randint(16, n-total_frames)
-
-            # Select the data around the sampled points
-            data_sel = data[the_key][idx:idx+total_frames, :]
-
-            # Add the data
-            encoder_inputs[i, :,
-                           0:self.input_size] = data_sel[0:self.source_seq_len-1, :]
-            decoder_inputs[i, :, 0:self.input_size] = data_sel[self.source_seq_len -
-                                                               1:self.source_seq_len+self.target_seq_len-1, :]
-            decoder_outputs[i, :,
-                            0:self.input_size] = data_sel[self.source_seq_len:, 0:self.input_size]
-
-        return encoder_inputs, decoder_inputs, decoder_outputs
-
-    def find_indices_srnn(self, data, action):
-        """
-        Find the same action indices as in SRNN.
-        See https://github.com/asheshjain399/RNNexp/blob/master/structural_rnn/CRFProblems/H3.6m/processdata.py#L325
-        """
-
-        # Used a fixed dummy seed, following
-        # https://github.com/asheshjain399/RNNexp/blob/srnn/structural_rnn/forecastTrajectories.py#L29
-        SEED = 1234567890
-        rng = np.random.RandomState(SEED)
-
-        subject = 5
-        subaction1 = 1
-        subaction2 = 2
-
-        T1 = data[(subject, action, subaction1, 'even')].shape[0]
-        T2 = data[(subject, action, subaction2, 'even')].shape[0]
-        prefix, suffix = 50, 100
-
-        idx = []
-        idx.append(rng.randint(16, T1-prefix-suffix))
-        idx.append(rng.randint(16, T2-prefix-suffix))
-        idx.append(rng.randint(16, T1-prefix-suffix))
-        idx.append(rng.randint(16, T2-prefix-suffix))
-        idx.append(rng.randint(16, T1-prefix-suffix))
-        idx.append(rng.randint(16, T2-prefix-suffix))
-        idx.append(rng.randint(16, T1-prefix-suffix))
-        idx.append(rng.randint(16, T2-prefix-suffix))
-        return idx
-
-    def get_batch_srnn(self, data, action):
-        """
-        Get a random batch of data from the specified bucket, prepare for step.
-        Args
-          data: dictionary with k:v, k=((subject, action, subsequence, 'even')),
-            v=nxd matrix with a sequence of poses
-          action: the action to load data from
-        Returns
-          The tuple (encoder_inputs, decoder_inputs, decoder_outputs);
-          the constructed batches have the proper format to call step(...) later.
-        """
-
-        actions = ["directions", "discussion", "eating", "greeting", "phoning",
-                   "posing", "purchases", "sitting", "sittingdown", "smoking",
-                   "takingphoto", "waiting", "walking", "walkingdog", "walkingtogether"]
-
-        if not action in actions:
-            raise ValueError("Unrecognized action {0}".format(action))
-
-        frames = {}
-        frames[action] = self.find_indices_srnn(data, action)
-
-        batch_size = 8  # we always evaluate 8 seeds
-        subject = 5  # we always evaluate on subject 5
-        source_seq_len = self.source_seq_len
-        target_seq_len = self.target_seq_len
-
-        seeds = [(action, (i % 2)+1, frames[action][i])
-                 for i in range(batch_size)]
-
-        encoder_inputs = np.zeros(
-            (batch_size, source_seq_len-1, self.input_size), dtype=float)
-        decoder_inputs = np.zeros(
-            (batch_size, target_seq_len, self.input_size), dtype=float)
-        decoder_outputs = np.zeros(
-            (batch_size, target_seq_len, self.input_size), dtype=float)
-
-        # Compute the number of frames needed
-        total_frames = source_seq_len + target_seq_len
-
-        # Reproducing SRNN's sequence subsequence selection as done in
-        # https://github.com/asheshjain399/RNNexp/blob/master/structural_rnn/CRFProblems/H3.6m/processdata.py#L343
-        for i in xrange(batch_size):
-
-            _, subsequence, idx = seeds[i]
-            idx = idx + 50
-
-            data_sel = data[(subject, action, subsequence, 'even')]
-
-            data_sel = data_sel[(idx-source_seq_len):(idx+target_seq_len), :]
-
-            encoder_inputs[i, :, :] = data_sel[0:source_seq_len-1, :]
-            decoder_inputs[i, :, :] = data_sel[source_seq_len -
-                                               1:(source_seq_len+target_seq_len-1), :]
-            decoder_outputs[i, :, :] = data_sel[source_seq_len:, :]
-
-        return encoder_inputs, decoder_inputs, decoder_outputs
