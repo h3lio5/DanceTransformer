d2375dff3954cc6cc8d7935f3bbcdc00c2265a41
h3li05369 <akashsm@ce.iitr.ac.in>
2020-01-08 02:02:05 +0530

    completed

--- tfp/utils/data_loader.py
+++ tfp/utils/data_loader.py
@@ -1,5 +1,9 @@
 import torch.utils.data as data
 import torch
+import json
+import numpy as np
+import math
+from tfp.config.config import SPLIT_JSON_LOC
 
 
 class PoseDataset(data.Dataset):
@@ -13,11 +17,15 @@ class PoseDataset(data.Dataset):
         frames :  collection of frames sequences
         shape  : (num_collections,sequence_length,dimension)
         """
-        self.data = self._get_data(args.location, args.seq_len, args.overlap,)
+        self.data = self._get_data(
+            args.location, args.seq_len, args.overlap, args.split_size, args.split, args.num_joints)
         # function
-        self._file_locations = self.get_files_locations()
         self.checkcomb = self.check_comb()
-        self.data = frames
+        self.sequence_length = args.seq_len
+        self.source_length = args.source_length
+        self.target_length = args.target_length
+        assert self.source_length + \
+            self.target_length != self.sequence_length, "Source length and target length don't sum upto sequence length"
 
     def ___len__(self):
         """
@@ -27,40 +35,122 @@ class PoseDataset(data.Dataset):
 
     def __getitem__(self, idx):
         """
-        returns: sequence of frames as FloatTensor object
+        Returns:
+            encoder_input: dance pose sequence input for encoder
+            decoder_input: dance pose sequence input for decoder
+            decoder_output: dance pose sequence used as target 
         """
         frame_seq = self.data[idx]
-        return torch.FloatTensor(frame_seq)
+        encoder_input = frame_seq[:self.source_length, :]
+        decoder_input = frame_seq[self.source_length:
+                                  self.source_length+self.target_length-1, :]
+        target = frame_seq[self.source_length +
+                           1:self.source_length+self.target_length, :]
+        return torch.FloatTensor(encoder_input), torch.FloatTensor(decoder_input), torch.FloatTensor(target)
 
-    def _get_data(self, args):
+    def _get_data(self, folder_location, sequence_length, overlap, split_size, split, num_joints):
         """
-        Returns
+        Args:
+            folder_location: location of the dataset folder
+            sequence_length: total number of dance pose frames in each batch
+            overlap: overlap between subsequent dance pose frames
+            split_size: percentage of frames in test dataset
+            split: train / test data
+            num_joints: number of joints in the pose
+
+        Returns:
+            data: total number of frame sequences
         """
+        # Unique identifier of the dataset
+        split_string = 'seq_len_'+str(sequence_length) + "_overlap" + \
+            str(overlap) + "_split_size" + str(split_size)
+        # Strides
+        strides = sequence_length - \
+            math.ceil(overlap * sequence_length / 100)
+        # All trial file locations
+        file_locations = self._get_files_locations(folder_location)
+        # combination present in the config file or not
+        comb_found = self._check_comb()
+        #======== Create train/test data =============#
+        if split == 'train':
+            if comb_found:
+                with open(SPLIT_JSON_LOC, 'r') as jsonfile:
+                    data = json.load(jsonfile)
+                    train_splits = data[self.split_string]['train_splits']
+            else:
+                train_splits, test_splits = self._generate_split(
+                    file_locations, split_size)
+                data = None
+                with open(SPLIT_JSON_LOC, "r") as jsonfile:
+                    data = json.load(jsonfile)
+                with open(SPLIT_JSON_LOC, "w") as jsonfile:
+                    data[split_string] = {"train_splits": list(
+                        train_splits), "test_splits": list(test_splits)}
+                    json.dump(data, jsonfile)
+            #
+            train_data = []
+            for file_name in train_splits:
+                file_loc = os.path.join(folder_location, file_name)
+                data = np.load(file_loc)
+                if data.shape[0] < sequence_length:
+                    break
+                num_batches = (data.shape[0] - sequence_length)//(strides) + 1
+                for i in range(num_batches):
+                    train_data.append(
+                        data[i * strides: sequence_length + i * strides])
 
+            return np.asarray(train_data).reshape(-1, sequence_length, num_joints*3)
 
-class Split:
-    r"""
-            This class generate Training data using the folder created by getData class
+        else:
+            if comb_found:
+                with open(SPLIT_JSON_LOC, 'r') as jsonfile:
+                    data = json.load(jsonfile)
+                    test_splits = data[self.split_string]['test_splits']
+            else:
+                train_splits, test_splits = self._generate_split(
+                    file_locations, split_size)
+                data = None
+                with open(SPLIT_JSON_LOC, "r") as jsonfile:
+                    data = json.load(jsonfile)
+                with open(SPLIT_JSON_LOC, "w") as jsonfile:
+                    data[split_string] = {"train_splits": list(
+                        train_splits), "test_splits": list(test_splits)}
+                    json.dump(data, jsonfile)
+            #
+            test_data = []
+            for file_name in test_splits:
+                file_loc = os.path.join(folder_location, file_name)
+                data = np.load(file_loc)
+                if data.shape[0] < sequence_length:
+                    break
+                num_batches = (data.shape[0] - sequence_length)//(strides) + 1
+                for i in range(num_batches):
+                    test_data.append(
+                        data[i * strides: sequence_length + i * strides])
 
-            returns an array of shape (None, seg_len, num_joints, 3)
-    """
+            return np.asarray(test_data).reshape(-1, sequence_length, num_joints*3)
 
-    def __init__(self, location=None, sequence_length=100, overlap=0, split_size=20):
-            # pramaters
-        self.folder_location = location
-        self.split_size = split_size
-        self.overlap = overlap
-        self.seq_len = sequence_length
-        self.split_string = str(sequence_length) + "_" + \
-            str(overlap) + "_" + str(split_size)
-        self.strides = self.seq_len - \
-            math.ceil(self.overlap * self.seq_len / 100)
-        # function
-        self.file_locations = self.get_files_locations()
-        self.checkcomb = self.check_comb()
+    def _generate_split(self, file_locations, split_size):
+        """ function to divide trails into train trials and split trails"""
 
-    def check_comb(self):
-        # json file should be present in config folder
+        num_test_trails = math.floor(len(file_locations) * (split_size)//100)
+        np.random.shuffle(file_locations)
+        train_split = file_locations[num_test_trails:]
+        test_split = file_locations[:num_test_trails]
+        return train_split, test_split
+
+    def _get_files_locations(self, folder_location):
+        """
+        Returns the locations of all the trial data present in our main data folder
+        """
+        all_numpy_files_loc = [x for x in os.listdir(
+            folder_location) if x[-3:] == "npy" or x[-3:] == "npz"]
+        return np.asarray(all_numpy_files_loc)
+
+    def _check_comb(self):
+        """
+        Checks whether json file is present in config folder
+        """
         found = False
         file = SPLIT_JSON_LOC
         with open(file) as jsonfile:
@@ -70,82 +160,3 @@ class Split:
             else:
                 found = False
         return found
-
-    def get_files_locations(self):
-        all_numpy_files_loc = [x for x in os.listdir(
-            self.folder_location) if x[-3:] == "npy" or x[-3:] == "npz"]
-        return np.asarray(all_numpy_files_loc)
-
-    def gen_split(self):
-        """ function to divide trials into train trials and split trails"""
-
-        files = self.file_locations
-
-        num_test_trails = math.floor(len(files) * (self.split_size)//100)
-        np.random.shuffle(files)
-        train_trails = files[num_test_trails:]
-        test_trails = files[:num_test_trails]
-
-        return train_trails, test_trails
-
-    def split_train(self):
-        if self.checkcomb:
-            with open(SPLIT_JSON_LOC, 'r') as jsonfile:
-                data = json.load(jsonfile)
-                train_splits = data[self.split_string]['train_splits']
-                test_splits = data[self.split_string]['test_splits']
-        else:
-            train_splits, test_splits = self.gen_split()
-            data = None
-            with open(SPLIT_JSON_LOC, "r") as jsonfile:
-                data = json.load(jsonfile)
-            with open(SPLIT_JSON_LOC, "w") as jsonfile:
-                data[self.split_string] = {"train_splits": list(
-                    train_splits), "test_splits": list(test_splits)}
-                json.dump(data, jsonfile)
-
-        comp_data = []
-        for _file in train_splits:
-            loc = os.path.join(self.folder_location, _file)
-            data = np.load(loc)
-            if data.shape[0] < self.seq_len:
-                break
-
-            num_bat = (data.shape[0] - self.seq_len)//(self.strides) + 1
-            print(data.shape)
-            print(num_bat)
-            for i in range(num_bat):
-                comp_data.append(
-                    data[i * self.strides: self.seq_len + i * self.strides])
-
-        return np.asarray(comp_data)
-
-    def split_test(self):
-        if self.checkcomb:
-            with open(SPLIT_JSON_LOC, 'r') as jsonfile:
-                data = json.load(jsonfile)
-                train_splits = data[self.split_string]['train_splits']
-                test_splits = data[self.split_string]['test_splits']
-        else:
-            train_splits, test_splits = self.gen_split()
-            data = None
-            with open(SPLIT_JSON_LOC, "r") as jsonfile:
-                data = json.load(jsonfile)
-            with open(SPLIT_JSON_LOC, "w") as jsonfile:
-                data[self.split_string] = {"train_splits": list(
-                    train_splits), "test_splits": list(test_splits)}
-                json.dump(data, jsonfile)
-
-        comp_data = []
-        for file in test_splits:
-            loc = os.path.join(self.folder_location, file)
-            data = np.load(loc)
-            if data.shape[0] < self.seq_len:
-                break
-
-            num_bat = (data.shape[0] - self.seq_len)//(self.strides) + 1
-            for i in range(num_bat):
-                comp_data.append(
-                    data[i * self.strides: self.seq_len + i * self.strides])
-
-            return np.asarray(comp_data)
