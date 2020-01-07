95b7462c46e13d0247a6d6f15832c19d9ef33219
h3li05369 <akashsm@ce.iitr.ac.in>
2020-01-07 22:30:06 +0530

    name changed

--- tfp/utils/data_loader.py
+++ tfp/utils/data_loader.py
@@ -2,26 +2,28 @@ import torch.utils.data as data
 import torch
 
 
-
-class MyDataset(data.Dataset):
+class PoseDataset(data.Dataset):
     """
 
     """
-    def __init__(self,frames):
+
+    def __init__(self, split="train"):
         """
         Parameter:
         frames :  collection of frames sequences
         shape  : (num_collections,sequence_length,dimension)
         """
+        if split == 'test':
+            self.data =
         self.data = frames
 
-    def ___len__(self) :
+    def ___len__(self):
         """
         returns: total number of collections of frame sequences
         """
         return len(self.data)
 
-    def __getitem__(self,idx):
+    def __getitem__(self, idx):
         """
         returns: sequence of frames as FloatTensor object
         """
