d474982ed5c37b8c7a71563817116d105838ed00
Atharv Maan <atharv24@live.in>
2020-01-09 22:45:39 +0530

    implement training loop

--- tfp/utils/data_loader.py
+++ tfp/utils/data_loader.py
@@ -3,6 +3,7 @@ import torch
 import json
 import numpy as np
 import math
+import os
 from tfp.config.config import SPLIT_JSON_LOC
 
 
@@ -12,19 +13,14 @@ class PoseDataset(data.Dataset):
     """
 
     def __init__(self, args):
-        """
-        Parameter:
-        frames :  collection of frames sequences
-        shape  : (num_collections,sequence_length,dimension)
-        """
         self.data = self._get_data(
-            args.location, args.seq_len, args.overlap, args.split_size, args.split, args.num_joints)
+            args.location, args.seq_len, args.overlap, args.split_ratio, args.split, args.num_joints)
         # function
         self.sequence_length = args.seq_len
         self.source_length = args.source_length
         self.target_length = self.sequence_length - self.source_length
 
-    def ___len__(self):
+    def __len__(self):
         """
         returns: total number of collections of frame sequences
         """
@@ -45,13 +41,13 @@ class PoseDataset(data.Dataset):
                            1:self.source_length+self.target_length, :]
         return torch.FloatTensor(encoder_input), torch.FloatTensor(decoder_input), torch.FloatTensor(target)
 
-    def _get_data(self, folder_location, sequence_length, overlap, split_size, split, num_joints):
+    def _get_data(self, folder_location, sequence_length, overlap, split_ratio, split, num_joints):
         """
         Args:
             folder_location: location of the dataset folder
             sequence_length: total number of dance pose frames in each batch
             overlap: overlap between subsequent dance pose frames
-            split_size: percentage of frames in test dataset
+            split_ratio: percentage of frames in test dataset
             split: train / test data
             num_joints: number of joints in the pose
 
@@ -59,24 +55,23 @@ class PoseDataset(data.Dataset):
             data: total number of frame sequences
         """
         # Unique identifier of the dataset
-        split_string = 'seq_len_'+str(sequence_length) + "_overlap" + \
-            str(overlap) + "_split_size" + str(split_size)
+        split_string = str(sequence_length) + '_' + str(overlap) + '_' + str(split_ratio)
         # Strides
         strides = sequence_length - \
             math.ceil(overlap * sequence_length / 100)
         # All trial file locations
         file_locations = self._get_files_locations(folder_location)
         # combination present in the config file or not
-        comb_found = self._check_comb()
+        comb_found = self._check_comb(split_string)
         #======== Create train/test data =============#
         if split == 'train':
             if comb_found:
                 with open(SPLIT_JSON_LOC, 'r') as jsonfile:
                     data = json.load(jsonfile)
-                    train_splits = data[self.split_string]['train_splits']
+                    train_splits = data[split_string]['train_splits']
             else:
                 train_splits, test_splits = self._generate_split(
-                    file_locations, split_size)
+                    file_locations, split_ratio)
                 data = None
                 with open(SPLIT_JSON_LOC, "r") as jsonfile:
                     data = json.load(jsonfile)
@@ -102,10 +97,10 @@ class PoseDataset(data.Dataset):
             if comb_found:
                 with open(SPLIT_JSON_LOC, 'r') as jsonfile:
                     data = json.load(jsonfile)
-                    test_splits = data[self.split_string]['test_splits']
+                    test_splits = data[split_string]['test_splits']
             else:
                 train_splits, test_splits = self._generate_split(
-                    file_locations, split_size)
+                    file_locations, split_ratio)
                 data = None
                 with open(SPLIT_JSON_LOC, "r") as jsonfile:
                     data = json.load(jsonfile)
@@ -127,10 +122,10 @@ class PoseDataset(data.Dataset):
 
             return np.asarray(test_data).reshape(-1, sequence_length, num_joints*3)
 
-    def _generate_split(self, file_locations, split_size):
+    def _generate_split(self, file_locations, split_ratio):
         """ function to divide trails into train trials and split trails"""
 
-        num_test_trails = math.floor(len(file_locations) * (split_size)//100)
+        num_test_trails = math.floor(len(file_locations) * (split_ratio))
         np.random.shuffle(file_locations)
         train_split = file_locations[num_test_trails:]
         test_split = file_locations[:num_test_trails]
@@ -144,7 +139,7 @@ class PoseDataset(data.Dataset):
             folder_location) if x[-3:] == "npy" or x[-3:] == "npz"]
         return np.asarray(all_numpy_files_loc)
 
-    def _check_comb(self):
+    def _check_comb(self, split_string):
         """
         Checks whether json file is present in config folder
         """
@@ -152,7 +147,7 @@ class PoseDataset(data.Dataset):
         file = SPLIT_JSON_LOC
         with open(file) as jsonfile:
             data = json.load(jsonfile)
-            if self.split_string in data.keys():
+            if split_string in data.keys():
                 found = True
             else:
                 found = False
